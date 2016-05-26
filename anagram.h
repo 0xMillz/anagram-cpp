f304a7614a96c4b1e6e8a763d650933688c4342a
millsmcilroy <millsmcilroy@gmail.com>
2016-05-26 10:17:28 -0600

    all tests passing

--- anagram.h
+++ anagram.h
@@ -9,43 +9,42 @@ private:
 
 public:
   anagram(std::string input) {
+    //Store input as all uppercase
+    for (int i = 0; i < input.length(); i++){
+      input[i] = toupper(input[i]);
+    }
+    std::cout << input << std::endl;
     this->word = input;
   };
 
   std::vector<std::string> matches(std::vector<std::string> input) {
 
+    //Vector to be returned
     std::vector<std::string> vect;
+    //Create thisCopy so this->word isnt destroyed
     std::string thisCopy = this->word;
+    //A var to build word to be compared
     std::string temp = "";
+    std::string upperInput = "";
     int pos = -1;
 
   for (int i = 0; i < input.size(); i++) {
     for (int j = 0; j < input[i].length(); j++) {
-      if (thisCopy.find(input[i][j]) != -1) {
-        pos = thisCopy.find(input[i][j]);
+      if (thisCopy.find(toupper(input[i][j])) != -1) {
+        pos = thisCopy.find(toupper(input[i][j]));
         temp += thisCopy[pos];
+        upperInput += toupper(input[i][j]);
         thisCopy.replace(pos,1,"");
       }
     }
-    //std::cout << "temp.length: "<< temp.length() << std::endl;
-    //std::cout << "inputi.length: "<< input[i].length() << std::endl;
-    //std::cout << "this->word.length: "<< this->word.length() << std::endl;
 
-    if (temp.length() == input[i].length() && this->word.length() == input[i].length()) {
+    if (temp.length() == input[i].length() && this->word.length() == input[i].length() && upperInput.compare(this->word) != 0) {
       vect.push_back(input[i]);
     }
     temp = "";
+    upperInput = "";
     thisCopy = this->word;
   }
   return vect;
  }
 };
-
-
-/*
-auto subject = anagram::anagram("ant");
-    auto matches = subject.matches({"tan", "stand", "at"});
-    vector<string> expected{"tan"};
-
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
-    */
